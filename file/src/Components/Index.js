b80b7f30735bde3aa5248b9e023cfc20cb6f28eb
sudhakar196435 <karankisudhakar12@gmail.com>
2025-03-21 13:49:58 +0530

    Update index page

--- file/src/Components/Index.js
+++ file/src/Components/Index.js
@@ -5,8 +5,6 @@ import PartnerLogos from './PartnerLogos';
 import '../Styles/Index.css';
 import heroImage from '../Assets/hero1.png';
 import servicesImage from '../Assets/image.png';
-import testimonialsImage from '../Assets/hero1.png';
-import contactImage from '../Assets/hero1.png';
 
 function Index() {
   return (
@@ -36,26 +34,6 @@ function Index() {
               <li>Leadership Development</li>
               <li>Employee Engagement Programs</li>
             </ul>
-          </div>
-        </div>
-
-        {/* Testimonials Section */}
-        <div className="section alternate">
-          <div className="section-content">
-            <h2>What Our Clients Say</h2>
-            <p className="testimonial">"Talentouch helped us streamline our hiring process and build a stronger, more cohesive team."<br /><span>- John Doe, CEO, XYZ Corp.</span></p>
-            <p className="testimonial">"The training programs provided by Talentouch have been a game changer for our organization."<br /><span>- Jane Smith, HR Director, ABC Ltd.</span></p>
-          </div>
-          <img src={testimonialsImage} alt="Testimonials" className="section-image" />
-        </div>
-
-        {/* Contact Section */}
-        <div className="section">
-          <img src={contactImage} alt="Contact Us" className="section-image" />
-          <div className="section-content">
-            <h2>Contact Us</h2>
-            <p>If you're interested in learning more about our services, feel free to reach out!</p>
-            <button className="cta-button">Get in Touch</button>
           </div>
         </div>
       </div>
